b709bb830436aed51a74c4fe72ca4ddd9d5ef837
Justin W Soto <soto@pdx.edu>
2021-06-30 16:22:30 -0700

    added basic style

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 // import functions and grab DOM elements
 
-//import {} from './utils.js';
+import { numCheck } from './utils.js';
 
-//  STATE for guess-a-number
-// const userGuess --> get this from the input field
-// const targetNumber --> get this using Math module
-// const guessesRemaining = 4
-
+const targetNum = Math.floor(Math.random() * 10);
 const userGuess = document.getElementById('input');
-const targetNum = Math.floor(Math.random()*10);
-const guessesRemaining = 4;
-
-const outPut = document.getElementById('output');
 const gBtn = document.getElementById('guess-btn');
-const rBtn = document.getElementById('reset-btn');
+//const rBtn = document.getElementById('reset-btn');
+const scr = document.getElementById('score');
+
+let guessesRemaining = 4;
 
 gBtn.addEventListener('click', () => {
-
+    guessesRemaining --;
+    // console.log(guessesRemaining);
+    // console.log(targetNum);
+    // console.log(userGuess);
+   
+    if (userGuess.value === targetNum) {
+        gBtn.disabled = true;
+    }
+    if (userGuess.value < targetNum) {
+        return numCheck.textContent = `Wrong Answer ${guessesRemaining}`;
+    }
+    if (userGuess.value > targetNum) {
+        return numCheck.textContent = `Wrong Answer ${guessesRemaining}`;
+    }
+    else
+        return numCheck.textContent = `You Are A Wizard!`;
 });
 
-rBtn.addEventListener('click', () => {
 
-});
+
+
+
+
+//rBtn.addEventListener('click', () => {
+
+//});
 
 //   get the userGuess from the input -- convert it to a number
 //   check the userGuess against the targetNumber
--- utils.js
+++ utils.js
@@ -1,1 +1,10 @@
-//export...
+//export
+const targetNum = Math.floor(Math.random() * 10);
+export function numCheck(submission) {
+    if (submission < targetNum) {
+        return 'Too Low';
+    } else if (submission > targetNum) {
+        return 'Too High';
+    } else  
+        return 'Congratulations, You Are A Wizard!';
+}
