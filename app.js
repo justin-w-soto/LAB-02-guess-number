91e452ce052a5e26b79414c7004879b62407ef6b
Justin W Soto <soto@pdx.edu>
2021-06-30 22:24:55 -0700

    late night overhaul of JS & CSS

--- app.js
+++ app.js
@@ -1,48 +1,78 @@
-// import functions and grab DOM elements
+
 //import { numCheck } from './utils.js';
-const targetNum = Math.floor(Math.random() * 10);
+const targetNum = Math.floor(Math.random() * 10) + 1;
+const guessField = document.getElementsByClassName('guess-field');
 const userGuess = document.getElementById('input');
 const gBtn = document.getElementById('guess-btn');
+const rBtn = document.getElementById('reset-btn');
 const scr = document.getElementById('score');
-//const rBtn = document.getElementById('reset-btn');
 let guessesRemaining = 4;
-gBtn.addEventListener('click', () => {
-    guessesRemaining --;
+function numCheck(){
+    let guessField = Number(userGuess.value);
+    if (guessesRemaining === 4) {
+        scr.textContent = 'previous guesses:';
+    }
+    scr.textContent += userGuess + '';
+
+    if (userGuess === targetNum) {
+        scr.textContent = 'You are a Wizard!';
+        scr.style.backgroundColor = 'teal';
+        scr.style.color = 'white';
+        setGameOver();
+    } else if (guessesRemaining === 0) {
+        scr.textContent = '!!!GAME OVER GROVER!!!';
+        setGameOver();
+    } else {
+        scr.textContent = 'Sorry, that is incorrect';
+        scr.style.backgroundColor = 'salmon';
+        scr.style.color = 'white';
+        if (userGuess < targetNum) {
+            scr.textContent = 'You guessed too low!';
+        } else if (userGuess > targetNum) {
+            scr.textContent = 'That was too high';
+        }
+    } 
+
+    guessesRemaining--;
+    userGuess.value = '';
+    userGuess.focus();
+}
+
+gBtn.addEventListener('click', numCheck, () => {
+    //guessesRemaining --;
     // console.log(guessesRemaining);
     // console.log(targetNum);
     // console.log(userGuess);
    
-    if (userGuess.value < targetNum) {
-        return scr.textContent = `Your Guess Was Too Low, But You Have ${guessesRemaining} More Tries`;
-    }
-    if (userGuess.value > targetNum) {
-        return scr.textContent = `Your Guess Was Too High, But You Have ${guessesRemaining} More Tries`;
-    }
-    else
-        return scr.textContent = `You Are A Wizard!`;
+    // if (userGuess.value < targetNum) {
+    //     return scr.textContent = `Your Guess Was Too Low, But You Have ${guessesRemaining} More Tries`;
+    // }
+    // if (userGuess.value > targetNum) {
+    //     return scr.textContent = `Your Guess Was Too High, But You Have ${guessesRemaining} More Tries`;
+    // }
+    // if (userGuess.value === targetNum) {
+    //     return scr.textContent = `You Are A Wizard!`;
+    // }
+    
 });
 
+function setGameOver() {
+    guessField.disabled = true;
+    gBtn.disabled = true;
+    rBtn.textContent = 'Start new game';
+    document.body.append(rBtn);
+    rBtn.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    guessesRemaining = 4;
+
+    rBtn.parentNode.removeChild(rBtn);
+
+    guessField.disabled = false;
+    gBtn.disabled = false;
+    guessField.value = '';
+    scr.style.backgroundColor = 'white'; 
+}
 
 
-
-// if (guessesRemaining.value < 1) {
-//   return scr.textContent = 'GAME OVER';
-// }
-
-
-//rBtn.addEventListener('click', () => {
-//});
-//   get the userGuess from the input -- convert it to a number
-//   check the userGuess against the targetNumber
-//   if userGuess === targetNumber --> 
-//       END THE GAME
-//       display winning message
-//   if userGuess !== targetNumber --> 
-//       display feedback with over or under
-//       decrement remaining guesses
-//          if no guesses remaining
-//             END THE GAME
-//             display losing message
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
